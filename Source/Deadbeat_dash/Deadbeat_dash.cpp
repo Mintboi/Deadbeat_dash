becc32cba732766d89acf9f849bbe9d8f5e1fd9a
Mintboi <martinpswift@gmail.com>
2025-04-29 15:34:54 +0100

    Added prologue level WIP

---
+++ Source/Deadbeat_dash/Deadbeat_dash.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Deadbeat_dash.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Deadbeat_dash, "Deadbeat_dash" );
